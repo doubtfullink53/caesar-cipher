982a275ddef744af5edaf67c97d393943b28b4f4
Robert <doubtfullink53@gmail.com>
2022-10-20 19:56:39 -0400

    add capitlized letters and merge with non capitilized

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,10 +1,16 @@
 def caesar_cipher (string, shift_factor = 1)
+    alphabet_non_capitilized = Array('a'..'z')
+    alphabet_capitilized = Array('A'..'Z')
 
-    numbers_letters = string.chars.map { |c| c.ord }
-    shifted_letters = numbers_letters.map { |c| c + shift_factor }
-    shifted_letters.map { |character| character.chr }.join
+
+    non_capitilized  = Hash[alphabet_non_capitilized.zip(alphabet_non_capitilized.rotate(shift_factor))]
+    capitilized  = Hash[alphabet_capitilized.zip(alphabet_capitilized.rotate(shift_factor))]
+
+
+    encrypted_merge = non_capitilized.merge(capitilized)
+
+    string.chars.map { |c| encrypted_merge.fetch(c, c) }.join
 end
-
 
 
 puts caesar_cipher("What a string!", 6)
